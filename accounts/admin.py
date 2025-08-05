48a992ce5d44b6cddce419211ef93d7526109fb8
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-08-05 23:34:29 +0100

    Add reviews to books

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,11 +9,46 @@ from .forms import CustomUserChangeForm, CustomUserCreationForm
 CustomUser = get_user_model()
 
 
-class CustomeUserAdmin(UserAdmin):
+class CustomerUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ["email", "username", "is_superuser"]
 
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
 
-admin.site.register(CustomUser, CustomeUserAdmin)
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "username",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomerUserAdmin)
