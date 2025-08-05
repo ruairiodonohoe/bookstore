259b275b056ffcfbf91d702379e52817db283554
Ruairi O'Donohoe <58832267+ruairiodonohoe@users.noreply.github.com>
2025-08-05 17:33:55 +0100

    SignUp testing

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,4 +18,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("pages.urls"))]
+urlpatterns = [
+    # Django admin
+    path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")),
+    path("", include("pages.urls")),
+]
